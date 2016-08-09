33f9ee32f2e276a463f4e76eb7f1c11f576bad67
Karthikeyan KC <karthikeyankc@live.com>
2016-08-09 18:06:46 +0530

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Tempinator
-A simple Python script to automatically clean the temporary folder on Windows.
+# Tempinator	
+A simple Python script that cleans the Temp directory in a Windows machine.
+## Description
+I wrote this script when my friend asked if there is a software available to clean the Windows temporary folder at startup.
---
+++ tempinator.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+print """
+Tempinator v0.1 by Karthikeyan KC
+A simple program written in Python that cleans the Temp directory in a Windows machine.
+"""
+temp_path = os.path.expanduser('~')+'\AppData\Local\\Temp'
+file_list = os.listdir(temp_path)
+for file in file_list:
+	file_path = os.path.join(temp_path, file)
+	if os.path.isdir(file_path):
+		shutil.rmtree(file_path, ignore_errors=True, onerror=None)
+	else:
+		try:
+			os.remove(file_path)
+		except WindowsError:
+			continue
+try:
+	if os.listdir(temp_path) == "":
+		print "Tempinator just terminated all files!"
+	else:
+		print "Some of these files couldn't be removed.\nTry closing any active applications and running it again.\n"
+		for file in os.listdir(temp_path):
+			print "%s" %file
+except:
+	"Directory not found!"
+raw_input("\nPress any key to exit!")
